81f3a7a2e2a5e0b7d67412456d4fcb59c8a9b4d3
Asterialumi <asterialumi@gmail.com>
2025-12-26 21:05:57 +0800

    added primitive data structures

--- DataStructures/main.cpp
+++ DataStructures/main.cpp
@@ -1,9 +1,8 @@
-#include "Tree/BinaryTree.hpp"
 #include <iostream>
 using namespace std;
 
 int main() {
-    
+    //empty for now, but this will be used for a future main menu to switch working from different data structures
 
-
+    return 0;
 }
