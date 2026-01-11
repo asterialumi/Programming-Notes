30f7c39b0aa2d34229ba37f506ec9ebb3cc68648
Asterialumi <asterialumi@gmail.com>
2026-01-11 14:44:22 +0800

    chore: working size() and capacity()

--- 02 Data Structures/Primitive/Array.hpp
+++ 02 Data Structures/Primitive/Array.hpp
@@ -4,57 +4,57 @@ using namespace std;
 
 class Array {
 public:
-    Array(): array(new int[0]), size(0), capacity(0), allocations(0) {}
+    Array(): array(new int[0]), _size(0), _capacity(0), allocations(0) {}
 
     //INSERT FUNCTIONS
     void add(int value) {
-        if(size == capacity) grow(1.6);
-        array[size++] = value;
+        if(_size == _capacity) grow(1.6);
+        array[_size++] = value;
     }
 
     void addAt(int index, int value) {
-        if(index < 0 || index >= size) return; //throw OutOfBounds
-        if(size == capacity) grow(1.6);
+        if(index < 0 || index >= _size) return; //throw OutOfBounds
+        if(_size == _capacity) grow(1.6);
 
-        for(int i = size; i > index; i--) {
+        for(int i = _size; i > index; i--) {
             array[i] = array[i-1];
         }
 
         array[index] = value;
-        size++;
+        _size++;
     }
 
     //REMOVE FUNCTIONS
     void remove() {
-        if(size == 0) return; //throw-->EmptyContainer
-        array[--size] = 0;
+        if(_size == 0) return; //throw-->EmptyContainer
+        array[--_size] = 0;
     }
 
     void removeAt(int index) {
-        if(index < 0 || index >= size) return; //throw-->OutOfBounds
+        if(index < 0 || index >= _size) return; //throw-->OutOfBounds
 
-        for(int i = index; i+1 < size; i++) {
+        for(int i = index; i+1 < _size; i++) {
             array[i] = array[i+1];
         }
 
-        array[--size] = 0;
+        array[--_size] = 0;
     }
 
     //ACCESS FUNCTIONS
     int get(int index) {
-        if(index < 0 || index >= size) return -1; //throw-->OutOfBounds
+        if(index < 0 || index >= _size) return -1; //throw-->OutOfBounds
         return array[index];
     }
     
-    int getSize() { return size; }
-    int getCapacity() { return capacity;}
+    int size() { return _size; }
+    int capacity() { return _capacity;}
 
     //DISPLAY FUNCTIONS
     void display() {
         cout << "[";
-        for(int i = 0; i < size; i++) {
+        for(int i = 0; i < _size; i++) {
             cout << array[i];
-            if(i+1 < capacity) cout << ",";
+            if(i+1 < _capacity) cout << ",";
         }
         cout << "]";
     }
@@ -63,68 +63,68 @@ public:
 
     //SPECIAL FUNCTIONS
     void set(int index, int value) {
-        if(index < 0 || index >= size) return; //throw-->OutOfBounds
+        if(index < 0 || index >= _size) return; //throw-->OutOfBounds
         array[index] = value;
     }
 
     void expand(int elements) {
-        if(size + elements > capacity) reserve(size + elements + 2);
+        if(_size + elements > _capacity) reserve(_size + elements + 2);
         
         for(int i = 0; i < elements; i++) {
-            array[i + size] = 0;
+            array[i + _size] = 0;
         }
 
-        size += elements;
+        _size += elements;
     }
 
     void resize(int elements) {
         if(elements < 0) return; //throw-->InvalidAmount
-        if(elements > capacity) reserve(elements + 2);
+        if(elements > _capacity) reserve(elements + 2);
 
-        for(int i = size - 1; i < elements; i++) {
+        for(int i = _size - 1; i < elements; i++) {
             array[i] = 0;
         }
 
-        for(int i = elements - 1; i < size; i++) {
+        for(int i = elements - 1; i < _size; i++) {
             array[i] = 0;
         }
 
-        size = elements;
+        _size = elements;
     }
     
     void reserve(int memory) {
-        if(memory < capacity) return; //throw-->InvalidAmount
+        if(memory < _capacity) return; //throw-->InvalidAmount
 
-        capacity = memory;
-        array = allocate(array, capacity);
+        _capacity = memory;
+        array = allocate(array, _capacity);
         allocations++;
     }
 
     void reduce(int memory) {
-        if(memory < size) return; //throw-->LengthUnderflow
-        if(memory > capacity) return; //throw-->InvalidAmount
+        if(memory < _size) return; //throw-->LengthUnderflow
+        if(memory > _capacity) return; //throw-->InvalidAmount
         
-        capacity = memory;
-        array = allocate(array, capacity);
+        _capacity = memory;
+        array = allocate(array, _capacity);
         allocations++;
     }
 
 private:
     int *array;
-    int size;
-    int capacity;
+    int _size;
+    int _capacity;
     int allocations;
 
     void grow(float factor) {
-        capacity = capacity == 0 ? 1: capacity * 1.5;
-        array = allocate(array, capacity);
+        _capacity = _capacity == 0 ? 2 : _capacity * 1.5;
+        array = allocate(array, _capacity);
         allocations++;
     }
 
     int* allocate(int* origin, int memory) {
         int* desti = new int[memory];
 
-        for(int i = 0; i < size; i++) {
+        for(int i = 0; i < _size; i++) {
             desti[i] = origin[i];
         }
 
